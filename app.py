1a5290e47ad3ed811b9bb12c9dae807c2d738e27
devsoni0419 <devsoni0419@gmail.com>
2025-10-16 12:21:39 +0530

    'dev'

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+from PIL import Image
+import google.generativeai as genai
 # st.header('AI MATHEMATICS ASSISTANT')
 html='''
 <style>
@@ -26,8 +28,20 @@ with st.sidebar:
         st.write('Instructions\n1. Upload an Image\n2. Click Solve Expression\n3. You will get your solution')
     if mode=='Chat':
         st.write('Instructions\n1. Ask chatbot about anything\n2. You will get your solution')
-    
-
-        
-
+def get_gemini_model(api_key,model = 'gemini-2.5-flash'):
+    try :
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel(model)
+        return model,None
+    except Exception as e:
+        return None ,str(e)
+def get_question():
+    ques = input("enter : ")
+    model,a = get_gemini_model(API_Key)
+    if model:
+        res = model.generate_content([ques])   
+        print(res.text) 
+if mode=='Chat':
+    get_gemini_model('GOOGLE_API_KEY')
+    get_question
 
